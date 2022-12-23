8ab1d80429ca2e4228fe634e7ac76dbaa203c129
vtutwane <vtutwane@singular.co.za>
2022-12-23 19:47:28 +0200

    Initial Commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MovieCard = ({Movie}) => {
+    return (
+        <div className="movie">
+            <div>
+                <p>{Movie.Year !== 'null' ? Movie.Year : 2022}</p>
+            </div>
+            <div>
+                <img src={Movie.Poster !=='N/A' ? Movie.Poster : 'https://via.placeholder.com/400'} alt={Movie.Title}/>
+            </div>
+
+            <div>
+                <span>{Movie.Type}</span>
+                <h3>{Movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
